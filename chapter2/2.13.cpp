a6ad7773b3c5b2deec951d6ed50046772ab1c126
ZengyuanYu <1952284157@qq.com>
2018-11-11 16:47:07 +0800

    update point reference

---
+++ chapter2/2.13.cpp
@@ -0,0 +1,12 @@
+//#include<iostream>
+//
+//int i = 42;
+//int main()
+//{
+//	int i = 100;
+//	int j = i;
+//	std::cout << j << std::endl;
+//	std::cout << i << std::endl;
+//	std::cout << ::i << std::endl;
+//	return 0;
+//}
---
+++ chapter2/2.3.cpp
@@ -0,0 +1,11 @@
+//#include<iostream>
+//
+//// 无符号和有符号的运算不能直接混用
+//int main()
+//{
+//	unsigned u = 10, u2 = 42;
+//	std::cout << u2 - u << std::endl;
+//	std::cout << u - u2 << std::endl;
+//	return 0;
+//
+//}
